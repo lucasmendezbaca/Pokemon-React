138f5b0a8fa576287f6e57ae19f30aed4dae21d4
Lucas Mendez <lucasmendezbaca@gmail.com>
2023-02-15 12:31:41 +0100

    .

---
+++ src/register/register.js
@@ -0,0 +1,11 @@
+function Register() {
+    return (
+        <div>
+            <h1>Register</h1>
+            <div>
+                <input type="email" placeholder="Nombre" />
+                <button>Registrarse</button>
+            </div>
+        </div>
+    )
+}
