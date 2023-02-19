38495710c626ab8c3a3820c50a3a78241373279e
Lucas Méndez <lucasmendezbaca@gmail.com>
2023-02-19 14:56:04 +0100

    gráfico estadísticas

--- src/detail/detail.js
+++ src/detail/detail.js
@@ -3,6 +3,10 @@ import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import './detail.css'
 
+import CanvasJSReact from '../canvas/canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 function Detail() {
     const typeClass = {
         grass: 'list-item__type--planta',
@@ -37,7 +41,6 @@ function Detail() {
         fetch('https://pokeapi.co/api/v2/pokemon/' + id)
         .then((response) => response.json())  
         .then((pokemonData) => {
-            console.log(pokemonData)
             setPokemon(pokemonData);
             setIsLoading(false)
         });
@@ -49,53 +52,73 @@ function Detail() {
 
     if(isLoading) {
         return <h3>cargando...</h3>
-    }
+    } else {
+        const defaultPoints = pokemon.stats.map( (stat, index) => {
+            return { label: stat.stat.name, y: stat.base_stat }
+        })
 
-    return (
-        <div className="detalle">
-            <div className='header--detalle'>
-                <h1>
-                    <span className='detalle__nombre'>{pokemon.name}</span>&nbsp;
-                    <span className="detalle__id">N.º {pokemon.id}</span>
-                </h1>
-            </div>
-            <div className='detalle__main'>
-                <div className='detalle__main__first-column'>
-                    <div className='detalle__main__first-column__img-container'>
-                        <img className='detalle__main__img' src={pokemon.sprites.other.home.front_default} />
-                    </div>
+        const options = {
+            title: {
+                text: "Base points"
+            },
+            data: [{				
+                    type: "column",
+                    dataPoints: defaultPoints
+            }]
+        }
+
+        return (
+            <div className="detalle">
+                <div className='header--detalle'>
+                    <h1>
+                        <span className='detalle__nombre'>{pokemon.name}</span>&nbsp;
+                        <span className="detalle__id">N.º {pokemon.id}</span>
+                    </h1>
                 </div>
-                <div className='detalle__main__second-column'>
-                    <div className='detalle__main__physical-characteristics'>
-                        <div className='detalle__main__weight'>
-                            <p className='detalle__main__physical-characteristics__title'>Peso:</p>
-                            <p>{withDecimal(pokemon.weight)} kg</p>
+                <div className='detalle__main'>
+                    <div className='detalle__main__first-column'>
+                        <div className='detalle__main__first-column__img-container'>
+                            <img className='detalle__main__img' src={pokemon.sprites.other.home.front_default} />
                         </div>
-                        <div className='detalle__main__height'>
-                            <p className='detalle__main__physical-characteristics__title'>Altura:</p>
-                            <p>{withDecimal(pokemon.height)} m</p>
+                    </div>
+                    <div className='detalle__main__second-column'>
+                        <div className='detalle__main__physical-characteristics'>
+                            <div className='detalle__main__weight'>
+                                <p className='detalle__main__physical-characteristics__title'>Peso:</p>
+                                <p>{withDecimal(pokemon.weight)} kg</p>
+                            </div>
+                            <div className='detalle__main__height'>
+                                <p className='detalle__main__physical-characteristics__title'>Altura:</p>
+                                <p>{withDecimal(pokemon.height)} m</p>
+                            </div>
+                            <div className='detalle__main__abilities'>
+                                <p className='detalle__main__physical-characteristics__title'>Habilidades:</p>
+                                {
+                                    pokemon.abilities.map( (ability, index) => <p key={index}>{ability.ability.name}</p>)
+                                }
+                            </div>
                         </div>
-                        <div className='detalle__main__abilities'>
-                            <p className='detalle__main__physical-characteristics__title'>Habilidades:</p>
+
+                        <div className='detalle__main__types'>
+                            <h3>Tipo:</h3>
                             {
-                                pokemon.abilities.map( (ability, index) => <p key={index}>{ability.ability.name}</p>)
+                                pokemon.types.map( (type, index) => <span className={`list-item__type ${typeClass[type.type.name]}`} key={index}>{type.type.name}</span>)
                             }
                         </div>
                     </div>
-
-                    <div className='detalle__main__types'>
-                        <h3>Tipo:</h3>
-                        {
-                            pokemon.types.map( (type, index) => <span className={`list-item__type ${typeClass[type.type.name]}`} key={index}>{type.type.name}</span>)
-                        }
-                    </div>
                 </div>
+            <div className='detail-chart'>
+                <CanvasJSChart options = {options}
+                    /* onRef = {ref => this.chart = ref} */
+                />
             </div>
-            <Link to="/pokedex">
-                <button className='boton'>Ir a la Pokedex</button>
-            </Link>
-        </div>
-    );
+                <Link to="/pokedex">
+                    <button className='boton'>Ir a la Pokedex</button>
+                </Link>
+            </div>
+        );
+
+    }
 }
 
 export default Detail;
