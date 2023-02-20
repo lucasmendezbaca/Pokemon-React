7eb8ea39db5df6ccfa6bf7df8d7e97d8541b4fee
Lucas Mendez <lucasmendezbaca@gmail.com>
2023-02-20 11:13:51 +0100

    estilos formulario registro

--- src/register/register.js
+++ src/register/register.js
@@ -24,15 +24,40 @@ function Register() {
         navigate('/pokedex');
     }
 
+    // return (
+    //     <div>
+    //         <h1>Register</h1>
+    //         <div>
+    //             <input type="email" placeholder="email" onChange={changeEmail} />
+    //             <input type="password" placeholder="password" onChange={changePassword} />
+    //             <button onClick={register}>Registrarse</button>
+    //             <p>{user.user}</p>
+    //         </div>
+    //     </div>
+    // )
+
     return (
-        <div>
-            <h1>Register</h1>
-            <div>
-                <input type="email" placeholder="email" onChange={changeEmail} />
-                <input type="password" placeholder="password" onChange={changePassword} />
-                <button onClick={register}>Registrarse</button>
-                <p>{user.user}</p>
+        <div className='account-form-container'>
+            <div className="registration" method="post">
+            <h1>👋 Welcome!</h1>
+
+            <label className="pure-material-textfield-outlined">
+                <input placeholder=" " type="email" required onChange={changeEmail}/>
+                <span>Email</span>
+            </label>
+
+            <label className="pure-material-textfield-outlined">
+                <input placeholder=" " type="password" required onChange={changePassword} />
+                <span>Password</span>
+            </label>
+
+            <button className="pure-material-button-contained" onClick={register}>Sign Up</button>
+
+            <div className="progress">
+                <progress className="pure-material-progress-circular" />
             </div>
+            </div>
+
         </div>
     )
 
